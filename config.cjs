bc349c15886add849169b300c43aa68beee48d2a
Roisteeve <167654420+Roisteeve@users.noreply.github.com>
2024-09-28 09:02:12 -0400

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Kerm v3~QGWvW1VZ"
+  SESSION_ID: process.env.SESSION_ID || "Kerm v3~2BFRAept"
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
