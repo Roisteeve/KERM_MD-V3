30ecf905d040dd5c5e2e31e41bff9465bbbc2815
Roisteeve <167654420+Roisteeve@users.noreply.github.com>
2024-10-05 11:59:14 -0400

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -19,8 +19,9 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "☞⌜ KG TECH⌝☜",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "237659535227",
+  OWNER_NAME: process.env.OWNER_NAME || "Zenitsu",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "50941469808"
+  
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
